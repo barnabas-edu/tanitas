8248670ed9e3b980ce62be7999f236def5ddf736
Horváth Barnabás <barnabas@teachers.org>
2025-09-14 18:52:16 +0200

    Update headerFooterManager.js

--- js/headerFooterManager.js
+++ js/headerFooterManager.js
@@ -8,28 +8,28 @@ class MyHeader extends HTMLElement {
                     <ul class="nav__list">
 												
                         <li class="nav__item">
-                            <a href="https://barnabas-edu.github.io/tanitas/" class="nav__link">
+                            <a href="/tanitas/" class="nav__link">
                                 <i class='bx bx-home-alt nav__icon'></i>
                                 <span class="nav__name">Kezdőlap</span>
                             </a>
                         </li>
 
                         <li class="nav__item">
-                            <a href="/tananyagok/" class="nav__link">
+                            <a href="/tanitas/tananyagok/" class="nav__link">
                                 <i class='bx bx-book nav__icon'></i>
                                 <span class="nav__name">Tananyagok</span>
                             </a>
                         </li>
 
                         <li class="nav__item">
-                            <a href="/szamologep/" class="nav__link">
+                            <a href="/tanitas/szamologep/" class="nav__link">
                                 <i class='bx bx-calculator nav__icon'></i>
                                 <span class="nav__name">Számológép</span>
                             </a>
                         </li>
 						
 						<li class="nav__item">
-                            <a href="/rolam/" class="nav__link">
+                            <a href="/tanitas/rolam/" class="nav__link">
                                 <i class='bx bx-user nav__icon'></i>
                                 <span class="nav__name">Rólam</span>
                             </a>
@@ -62,6 +62,7 @@ class MyFooter extends HTMLElement {
 }
 
 customElements.define('my-footer', MyFooter)
+
 
 
 
