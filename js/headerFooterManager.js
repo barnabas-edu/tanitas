6cbb938774311bcd2e144b5633248c79da4837c5
Horváth Barnabás <barnabas@teachers.org>
2025-09-14 15:59:07 +0200

    Update headerFooterManager.js

--- js/headerFooterManager.js
+++ js/headerFooterManager.js
@@ -3,33 +3,33 @@ class MyHeader extends HTMLElement {
 		this.innerHTML = `
 		<header class="header" id="header">
             <nav class="nav container">
-                <a href="index.html" class="nav__logo">Horváth Barnabás Oldala</a>
+                <a href="/" class="nav__logo">Horváth Barnabás Oldala</a>
                 <div class="nav__menu" id="nav-menu">
                     <ul class="nav__list">
 												
                         <li class="nav__item">
-                            <a href="index.html" class="nav__link">
+                            <a href="/" class="nav__link">
                                 <i class='bx bx-home-alt nav__icon'></i>
                                 <span class="nav__name">Kezdőlap</span>
                             </a>
                         </li>
 
                         <li class="nav__item">
-                            <a href="/tananyagok/index.html" class="nav__link">
+                            <a href="/tananyagok/" class="nav__link">
                                 <i class='bx bx-book nav__icon'></i>
                                 <span class="nav__name">Tananyagok</span>
                             </a>
                         </li>
 
                         <li class="nav__item">
-                            <a href="/szamologep/index.html" class="nav__link">
+                            <a href="/szamologep/" class="nav__link">
                                 <i class='bx bx-calculator nav__icon'></i>
                                 <span class="nav__name">Számológép</span>
                             </a>
                         </li>
 						
 						<li class="nav__item">
-                            <a href="/rolam/index.html" class="nav__link">
+                            <a href="/rolam/" class="nav__link">
                                 <i class='bx bx-user nav__icon'></i>
                                 <span class="nav__name">Rólam</span>
                             </a>
@@ -62,4 +62,5 @@ class MyFooter extends HTMLElement {
 }
 
 customElements.define('my-footer', MyFooter)
+
 
