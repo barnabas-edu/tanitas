4086f6a9ca83d4f5653e4d1b3ac76aa659c705d6
Horváth Barnabás <barnabas@teachers.org>
2025-09-14 15:53:20 +0200

    Update headerFooterManager.js

--- js/headerFooterManager.js
+++ js/headerFooterManager.js
@@ -15,21 +15,21 @@ class MyHeader extends HTMLElement {
                         </li>
 
                         <li class="nav__item">
-                            <a href="tananyagok.html" class="nav__link">
+                            <a href="/tananyagok/index.html" class="nav__link">
                                 <i class='bx bx-book nav__icon'></i>
                                 <span class="nav__name">Tananyagok</span>
                             </a>
                         </li>
 
                         <li class="nav__item">
-                            <a href="szamologep.html" class="nav__link">
+                            <a href="/szamologep/index.html" class="nav__link">
                                 <i class='bx bx-calculator nav__icon'></i>
                                 <span class="nav__name">Számológép</span>
                             </a>
                         </li>
 						
 						<li class="nav__item">
-                            <a href="rolam.html" class="nav__link">
+                            <a href="/rolam/index.html" class="nav__link">
                                 <i class='bx bx-user nav__icon'></i>
                                 <span class="nav__name">Rólam</span>
                             </a>
@@ -62,3 +62,4 @@ class MyFooter extends HTMLElement {
 }
 
 customElements.define('my-footer', MyFooter)
+
