e18bd0876c019d50f8e3ba072abc97a2d05d07f6
Horváth Barnabás <barnabas@teachers.org>
2025-09-14 15:37:41 +0200

    Update headerFooterManager.js

--- js/headerFooterManager.js
+++ js/headerFooterManager.js
@@ -3,44 +3,49 @@ class MyHeader extends HTMLElement {
 		this.innerHTML = `
 		<header class="header" id="header">
             <nav class="nav container">
-                <a href="/tanitas/" class="nav__logo">Horváth Barnabás Oldala</a>
-
+                <a href="index.html" class="nav__logo">Horváth Barnabás Oldala</a>
                 <div class="nav__menu" id="nav-menu">
                     <ul class="nav__list">
+												
                         <li class="nav__item">
-                            <a href="/tanitas/" class="nav__link">
+                            <a href="index.html" class="nav__link">
                                 <i class='bx bx-home-alt nav__icon'></i>
                                 <span class="nav__name">Kezdőlap</span>
                             </a>
                         </li>
 
                         <li class="nav__item">
-                            <a href="/tanitas/tananyagok/" class="nav__link">
+                            <a href="tananyagok.html" class="nav__link">
                                 <i class='bx bx-book nav__icon'></i>
                                 <span class="nav__name">Tananyagok</span>
                             </a>
                         </li>
 
                         <li class="nav__item">
-                            <a href="/tanitas/segedletek/" class="nav__link">
-                                <i class='bx bx-file-blank nav__icon'></i>
-                                <span class="nav__name">Segédletek</span>
+                            <a href="szamologep.html" class="nav__link">
+                                <i class='bx bx-calculator nav__icon'></i>
+                                <span class="nav__name">Számológép</span>
                             </a>
                         </li>
 						
 						<li class="nav__item">
-                            <a href="/tanitas/rolam/" class="nav__link">
+                            <a href="rolam.html" class="nav__link">
                                 <i class='bx bx-user nav__icon'></i>
                                 <span class="nav__name">Rólam</span>
                             </a>
                         </li>
                     </ul>
                 </div>
+				<button id="theme-toggle">
+				<i class="bx"></i>
+				</button>
             </nav>
         </header>
 		`
 	}
 }
+
+
 
 customElements.define('my-header', MyHeader)
 
@@ -49,8 +54,8 @@ class MyFooter extends HTMLElement {
 		this.innerHTML = `
 		<center><footer>
 		<nav>
-        <li><a href="/tanitas/index.html">Kezdőlap</a></li>
-        <li><a href="/tanitas/altkem.html">Általános kémia</a></li>
+        <li><a href="index.html">Kezdőlap</a></li>
+        <li><a href="altkem.html">Általános kémia</a></li>
         </nav></footer><center>
 		`
 	}
