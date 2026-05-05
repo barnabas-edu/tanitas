8aa4aed920105b2d03888b83a1f72b31fd45d24b
Horváth Barnabás <barnabas@teachers.org>
2026-05-05 07:05:55 +0200

    Update headerFooterManager.js

--- js/headerFooterManager.js
+++ js/headerFooterManager.js
@@ -2,11 +2,11 @@ class MyHeader extends HTMLElement {
 	connectedCallback() {
 		this.innerHTML = `
 		<header class="header" id="header">
-            <nav class="nav container">
-                <a href="https://barnabas-edu.github.io/tanitas/" class="nav__logo">Horváth Barnabás Oldala</a>
+        <div class="navbar">
+                <div><a href="https://barnabas-edu.github.io/tanitas/" class="nav__logo">Horváth Barnabás Oldala</a></div>
                 <div class="nav__menu" id="nav-menu">
                     <ul class="nav__list">
-												
+
                         <li class="nav__item">
                             <a href="/tanitas/" class="nav__link">
                                 <i class='bx bx-home-alt nav__icon'></i>
@@ -36,16 +36,31 @@ class MyHeader extends HTMLElement {
                         </li>
                     </ul>
                 </div>
-				<button id="theme-toggle">
-				<i class="bx"></i>
-				</button>
-            </nav>
-        </header>
+        <div class="theme-button" id="themeMenuButton">
+            <i class='bx bx-palette nav__icon'></i>
+        </div>
+
+        <div class="popup" id="themePopup">
+            <!-- Mód választó -->
+            <div class="section-title" style="text-align: center">Mód</div>
+            <div class="option-group" id="modeOptions">
+				<div class="option" data-mode="auto"><span class="material-symbols-outlined">routine</span> Auto</div>
+                <div class="option" data-mode="light"><span class="material-symbols-outlined">light_mode</span> Világos</div>
+                <div class="option" data-mode="dark"><span class="material-symbols-outlined">dark_mode</span> Sötét</div>
+            </div>
+
+            <div class="section-title" style="text-align: center">Szín</div>
+            <div style="display: flex; align-items: center; justify-content: center; gap: 0.5rem;" id="colorOptions">
+                <span class="color-option" data-color="default" title="Alapértelmezett"></span>
+                <span class="color-option" data-color="blue" title="Kék"></span>
+                <span class="color-option" data-color="green" title="Zöld"></span>
+				<span class="color-option" data-color="green" title="Valami"></span>
+            </div>
+        </div>
+    </header>
 		`
 	}
 }
-
-
 
 customElements.define('my-header', MyHeader)
 
@@ -62,7 +77,3 @@ class MyFooter extends HTMLElement {
 }
 
 customElements.define('my-footer', MyFooter)
-
-
-
-
